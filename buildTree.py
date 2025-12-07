6d4007e1eeebb10bfd33c121a943d3502cad8f24
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-06 22:34:52 -0600

    Add TreeNode class for binary tree construction

--- buildTree.py
+++ buildTree.py
@@ -2,3 +2,9 @@
 # 105. Construct Binary Tree from Preorder and Inorder Traversal
 # Definition for a binary tree node.
 from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
