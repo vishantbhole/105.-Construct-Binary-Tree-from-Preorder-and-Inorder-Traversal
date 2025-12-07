5d739ff92157a447c9319e8a361f8d9d2e18a606
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-06 22:35:32 -0600

    Add Solution class with buildTree method

--- buildTree.py
+++ buildTree.py
@@ -8,3 +8,9 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        if not preorder or not inorder:
+            return None
